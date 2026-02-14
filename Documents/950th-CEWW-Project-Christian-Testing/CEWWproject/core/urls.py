faac3219787389e756b111a2ca4a19bec524a240
Christian Lambino <chdi.lambino.up@phinamed.com>
2026-02-15 01:46:32 +0800

    Ticketing is halfway done more on details and other implementation that need to put

--- Documents/950th-CEWW-Project-Christian-Testing/CEWWproject/core/urls.py
+++ Documents/950th-CEWW-Project-Christian-Testing/CEWWproject/core/urls.py
@@ -34,11 +34,10 @@ urlpatterns = [
     path('assets/edit/<int:asset_id>/', views.edit_asset, name='edit_asset'),
     path('assets/delete/<int:asset_id>/', views.delete_asset, name='delete_asset'),
     path("admin_tickets/", views.admin_ticket_dashboard, name="admin_tickets"),
+    path('manage/ticket/<int:ticket_id>/', views.ticket_detail, name='ticket_detail'),
     path("submit_ticket/", views.submit_ticket, name="submit_ticket"),
     path("command_submit_ticket/", views.command_submit_ticket, name="command_submit_ticket"),
-    # Admin views
-    path("admin_tickets/", views.admin_ticket_dashboard, name="admin_tickets"),
-    path('manage/ticket/<int:ticket_id>/', views.ticket_detail, name='ticket_detail'),
+
 
 
 
