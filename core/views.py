1d31e147e46d609ff3abd5a97235040025b03ff0
Honrad Gallardo <hooc.gallardo.up@phinmaed.com>
2026-04-07 19:25:14 +0800

    Added new user role REGULAR

--- core/views.py
+++ core/views.py
@@ -80,6 +80,8 @@ def dashboard(request):
         return render(request, 'core/Personnel/personnel_dashboard.html', context)
     elif 'Admin' in user_groups or request.user.is_superuser:
         return render(request, 'core/Admin/admin_dashboard.html', context)
+    elif 'Regular' in user_groups or request.user.is_superuser:
+        return render(request, 'tickets/submit_ticket.html', context)
 
     # Final Fallback to prevent ValueError
     return render(request, 'core/Admin/admin_dashboard.html', context)
