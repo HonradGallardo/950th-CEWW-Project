0db144b7a4172ef220736ef042f2ee018f76f735
Honrad Gallardo <hooc.gallardo.up@phinmaed.com>
2026-04-19 22:27:00 +0800

    Implementing Frontend Security

--- CEWWproject/settings.py
+++ CEWWproject/settings.py
@@ -36,9 +36,11 @@ DEBUG = env.bool('DEBUG', default=False)
 
 # SECURITY FIX: Commented out the wildcard '*' to prevent Host Header attacks.
 # ALLOWED_HOSTS = ['*'] 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['127.0.0.1', 'localhost', 'onthego-aims.onrender.com'])
-# -------------------------------
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['127.0.0.1', 'localhost', 'onthego-aims.up.railway.app'])
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://onthego-aims.up.railway.app',
+]
 # 5. Application Definition
 INSTALLED_APPS = [
     'django.contrib.admin',
